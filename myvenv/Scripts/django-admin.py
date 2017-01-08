3c7c5b9091a66899e1aa600558146ccdfc21ea9f
Michael <michael.rsb076@gmail.com>
2017-01-08 16:24:22 +0100

    New app, with sqllite database

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\s157800\memoapp\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ poll/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Question(models.Model):
+    author = models.CharField(max_length=1000)
+    question_text=models.CharField(max_length=1000)
+    pub_date=models.DateTimeField(default=timezone.now)
+    likes = models.PositiveIntegerField(default=0)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
